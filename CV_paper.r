b4b980a78768b37bce78adda071acc3f166c85ab
Chum_mul <40909985+waterfirst@users.noreply.github.com>
2024-10-23 15:26:15 +0900

    Update CV_paper.r

--- CV_paper.r
+++ CV_paper.r
@@ -14,4 +14,6 @@ Currently, working on micro display and micro LED development.
 Jae Beom Choi received his BS and MS degrees in Material Science and Engineering from Korea Advanced Institute of Science and Technology (KAIST) in 1992 and 1994, respectively. Then, he received Ph.D degree in Applied Physics from Columbia University in 2005 and joined Samsung. Since 2005, he developed LTPS LCD/OLED and large area OLED displays. He also led the product planning team of mobile OLED division for new mobile devices. In particular, he led QD-OLED process development team and successfully launched QD-OLED TV in 2021. Currently, as a team leader of the micro display team, he is in charge of development of white and RGB OLEDoS and Micro-LED watch for the future market.
 
 
- 
+ 안상우
+
+Sangwoo An received his bachelor's degree in Applied Electronics and Communications Engineering Department from Hanyang University in 2011, master's degree in Applied semiconductor process from Hanyang University in 2013 Currently, he is a responsible Engineer at Samsung Display, Project PA. His research is focused on the development of advanced display technologies, including OLED, and Micro LED displays
