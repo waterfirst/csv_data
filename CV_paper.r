cc0ec3f99013545365964cb40ed6de65c885b24e
Chum_mul <40909985+waterfirst@users.noreply.github.com>
2024-10-23 15:05:49 +0900

    Create CV_paper.r

---
+++ CV_paper.r
@@ -0,0 +1,17 @@
+CV
+
+허종무
+Jong-moo Huh received his Ph. D. of Material Science Engineering degree in 2000 at Korea Advanced Institute of Science and Technology, Korea.
+
+For two years, participated in a crystal growth project as a visiting researcher at National Institute of Standards and Technology, USA.
+
+Since 2002, Developed Oxide TFT, Hybrid TFT Back-Plane, AMOLED, White OLED, and Flexible Display at Samsung Display Company.
+
+Currently, working on micro display and micro LED development.
+
+최재범
+
+Jae Beom Choi received his BS and MS degrees in Material Science and Engineering from Korea Advanced Institute of Science and Technology (KAIST) in 1992 and 1994, respectively. Then, he received Ph.D degree in Applied Physics from Columbia University in 2005 and joined Samsung. Since 2005, he developed LTPS LCD/OLED and large area OLED displays. He also led the product planning team of mobile OLED division for new mobile devices. In particular, he led QD-OLED process development team and successfully launched QD-OLED TV in 2021. Currently, as a team leader of the micro display team, he is in charge of development of white and RGB OLEDoS and Micro-LED watch for the future market.
+
+
+ 
